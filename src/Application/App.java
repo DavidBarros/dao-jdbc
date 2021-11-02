5a257532c18fadaeb2d8fe68b2426033f760663f
DavidBarros <DavidB4rros@gmail.com>
2021-11-02 01:37:03 -0300

    Seller Entity Class

--- src/Application/App.java
+++ src/Application/App.java
@@ -1,11 +1,19 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
        
         Department obj = new Department(1, "Book");
-        System.out.println(obj);
+        
+    
+        Seller seller = new Seller(21, "Luzia","luzia@gmail.com" , new Date(), 3200.00, obj);
+
+        System.out.println(seller);
+    
     }
 }
